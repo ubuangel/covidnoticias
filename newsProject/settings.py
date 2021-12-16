a39699c8ac848f12d5bd82aeed29213d71573c6c
angel <andresbjar97@gmail.com>
2021-12-16 18:58:48 -0500

    agregando a gitignore

--- newsProject/settings.py
+++ newsProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jlu03617k&_%xd09!@dlfl0k%qgmdn2g0h2y$wx6b0yl&-p!mm'
+#SECRET_KEY = 'django-insecure-jlu03617k&_%xd09!@dlfl0k%qgmdn2g0h2y$wx6b0yl&-p!mm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
