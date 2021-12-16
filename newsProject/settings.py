7df106cd339e65bc276b6bf139120c578dee1927
angel <andresbjar97@gmail.com>
2021-12-16 18:53:05 -0500

    Automatic configuration with django-heroku

--- newsProject/settings.py
+++ newsProject/settings.py
@@ -128,3 +128,5 @@ MEDIA_URL = '/images/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import django_heroku
+django_heroku.settings(locals())
